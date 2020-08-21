3eb73f5c7200b9bb991bebe44c99ae905ba5fd0e
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-08-21 16:11:55 +0800

    Implement UpdateItem

--- martian-library/app/graphql/types/mutation_type.rb
+++ martian-library/app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :sign_in, mutation: Mutations::SignInMutation
     field :sign_out, mutation: Mutations::SignOutMutation
     field :add_item, mutation: Mutations::AddItemMutation
+    field :update_item, mutation: Mutations::UpdateItemMutation
   end
 end
