3593850833dd523ff6fff442c899a0e2ac727f34
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-08-21 16:01:19 +0800

    Add Library View

--- martian-library/app/javascript/components/Library/index.jsx
+++ martian-library/app/javascript/components/Library/index.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import { Query } from 'react-apollo';
+import React, { useState } from "react";
+import { Query } from "react-apollo";
 import { LibraryQuery } from "./operations.graphql";
+import cs from "./styles";
 
-export default () => (
-  <Query query={LibraryQuery}>
-    {({ data, loading }) => (
-      <div>
-        {loading
-          ? 'loading...'
-          : data.items.map(({ title, id, user }) => (
-              <div key={id}>
-                <b>{title}</b> {user ? `added by ${user.email}` : null}
-              </div>
+const Library = () => {
+  const [item, setItem] = useState(null);
+  return (
+    <Query query={LibraryQuery}>
+      {({ data, loading }) => (
+        <div className={cs.library}>
+          {loading || !data.items
+            ? "loading..."
+            : data.items.map(({ title, id, user, imageUrl, description }) => (
+              <button
+                key={id}
+                className={cs.plate}
+                onClick={() => setItem({ title, imageUrl, id, description })}
+              >
+                <div className={cs.title}>{title}</div>
+                <div>{description}</div>
+                {imageUrl && <img src={imageUrl} className={cs.image} />}
+                {user ? (
+                  <div className={cs.user}>added by {user.email}</div>
+                ) : null}
+              </button>
             ))}
-      </div>
-    )}
-  </Query>
-);
+        </div>
+      )}
+    </Query>
+  );
+};
+
+export default Library;
