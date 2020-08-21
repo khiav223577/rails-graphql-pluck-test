ca18e57d714751b4bef8b4fc4ee13e3ef2431a4d
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-08-21 15:20:24 +0800

    Add sign_in feature

--- martian-library/app/javascript/packs/index.jsx
+++ martian-library/app/javascript/packs/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
+import UserInfo from '../components/UserInfo';
 import Library from '../components/Library';
 
 render(
   <Provider>
+    <UserInfo />
     <Library />
   </Provider>,
   document.querySelector('#root')
