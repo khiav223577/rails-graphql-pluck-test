c4f7e7f454de48a2ef35b0cb1a7a91fd30774830
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-08-21 16:06:32 +0800

    Implement AddItem

--- martian-library/app/graphql/types/mutation_type.rb
+++ martian-library/app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :sign_in, mutation: Mutations::SignInMutation
     field :sign_out, mutation: Mutations::SignOutMutation
+    field :add_item, mutation: Mutations::AddItemMutation
   end
 end
--- martian-library/app/javascript/packs/index.jsx
+++ martian-library/app/javascript/packs/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
 import UserInfo from '../components/UserInfo';
+import AddItemForm from '../components/AddItemForm';
 import Library from '../components/Library';
 
 render(
   <Provider>
     <UserInfo />
+    <AddItemForm />
     <Library />
   </Provider>,
   document.querySelector('#root')
