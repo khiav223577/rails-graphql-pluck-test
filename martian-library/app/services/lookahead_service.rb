e504a58f08c32998dca990faeb72fb8f190e8dd2
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-08-23 00:59:43 +0800

    try to ignore fields that are not the column of models

--- martian-library/app/services/lookahead_service.rb
+++ martian-library/app/services/lookahead_service.rb
@@ -1,12 +1,18 @@
 class LookaheadService
   def initialize(lookahread)
     @lookahread = lookahread
+    @type_to_custom_fields = {}
   end
 
   def to_query_params
     @lookahread.selections.map do |selection|
+      next if selection.name.in?(custom_fields_of(selection.owner_type))
       next selection.name if selection.selections.empty?
       next { selection.name => LookaheadService.new(selection).to_query_params }
-    end
+    end.compact
+  end
+
+  def custom_fields_of(type)
+    @type_to_custom_fields[type] ||= type.instance_methods & type.fields.keys.map{|s| s.underscore.to_sym }
   end
 end
